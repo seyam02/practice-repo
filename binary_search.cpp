dee1db21bdfe39629bfd68e1e120cfa68305ba55
MD Khalid Saifullah Seyam <198321769+seyam02@users.noreply.github.com>
2026-04-16 00:12:57 +0600

    Add files via upload

---
+++ binary_search.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {3, 4, 12, 23, 32, 40};
+    int n = 6;
+    int key = 4;
+
+    int low = 0, high = n - 1;
+    int mid;
+    int found = -1;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            found = mid;
+            break;
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if (found != -1)
+        cout << "Found at index " << found << endl;
+    else
+        cout << "Not Found" << endl;
+
+    return 0;
+}
